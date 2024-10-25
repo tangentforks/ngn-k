91dcfda05e2ce83438a31178951294d24754b743
Douglas Mennella <douglas.mennella@gmail.com>
2024-10-25 15:55:25 +0900

    More coverage for foreign tests

--- x/t/t.c
+++ x/t/t.c
@@ -10,7 +10,15 @@ A6(Fadd6,Ki(iK(x1)+iK(x2)+iK(x3)+iK(x4)+iK(x5)+iK(x6)))
 A7(Fadd7,Ki(iK(x1)+iK(x2)+iK(x3)+iK(x4)+iK(x5)+iK(x6)+iK(x7)))
 A8(Fadd8,Ki(iK(x1)+iK(x2)+iK(x3)+iK(x4)+iK(x5)+iK(x6)+iK(x7)+iK(x8)))
 
+// Allocate with a null pointer
+A2(FKC0,I n=iK(x);C v=cK(y);K r=KC(0,n);C* a=(C*) r;F(n,a[i]=v);r)
+A2(FKI0,I n=iK(x);I v=iK(y);K r=KI(0,n);I* a=(I*) r;F(n,a[i]=v);r)
+A2(FKF0,I n=iK(x);F v=fK(y);K r=KF(0,n);F* a=(F*) r;F(n,a[i]=v);r)
+A2(FKL0,I n=iK(x);          K r=KL(0,n);K* a=(K*) r;F(n,a[i]=y;ref(y));y(r))
+
 #define REG(a,n) KR(#a,F##a,n);
 
 __attribute__((constructor)) void loadfns();
-void loadfns(){REG(add1,1)REG(add2,2)REG(add3,3)REG(add4,4)REG(add5,5)REG(add6,6)REG(add7,7)REG(add8,8)}
+void loadfns(){REG(add1,1)REG(add2,2)REG(add3,3)REG(add4,4)
+               REG(add5,5)REG(add6,6)REG(add7,7)REG(add8,8)
+               REG(KC0,2)REG(KI0,2)REG(KF0,2)REG(KL0,2)}
--- x/t/t.h
+++ x/t/t.h
@@ -1,7 +1,15 @@
 #include "unistd.h"
 #include "k.h"
 
-#define    _(a...) {return({a;});}
+#define TY __typeof__
+
+#define x(a) ({K r_=(a);unref(x);r_;})
+#define y(a) ({K r_=(a);unref(y);r_;})
+#define z(a) ({K r_=(a);unref(z);r_;})
+
+#define _(a...) {return({a;});}
+#define F_(i,n,a...) for(TY(n)n_=(n),i=0;i<n_;i++){a;}
+#define F(a...) F_(i,a)
 
 #define A1(f,b...) K f(K x             )_(b)/*1*/
 #define A2(f,b...) K f(K x,K y         )_(b)/*01*/
@@ -11,4 +19,3 @@
 #define A6(f,b...) K f(K x1,K x2,K x3,K x4,K x5,K x6           )_(b)
 #define A7(f,b...) K f(K x1,K x2,K x3,K x4,K x5,K x6,K x7      )_(b)
 #define A8(f,b...) K f(K x1,K x2,K x3,K x4,K x5,K x6,K x7,K x8 )_(b)
-
