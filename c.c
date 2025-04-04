028d70198555c9b7e1b6d7db554080b2c7cd240b
Douglas Mennella <douglas.mennella@gmail.com>
2025-04-04 18:52:13 +0900

    char parsing should recurse too.

--- c.c
+++ c.c
@@ -25,7 +25,7 @@ Z X1(prsB,RmMA(e1f(prsB,x))Rc(prsB(enl(x)))RC(A u=aV(tL,xn+7>>3,xV);uT=tB;un=xn<
 Z X1(prsI,RmMA(e1f(prsI,x))Rc(prsI(enl(x)))RC(x=str0(x);S s=xV;P(!*s,x(_R(cn[tl])))L v=pl(&s);x(*s?_R(cn[tl]):az(v)      ))
 	 RB(N n=(xn-1+(1<<6))>>6,m=xn;A u=MINE(x)?xR:aV(tL,n,xV);uT=tL;un=n;I(m%64,uL[un-1]&=-1+(1ull<<(m%64u)));x(u))R_(et(x)))
 Z X1(prsF,RmMA(e1f(prsF,x))Rc(prsF(enl(x)))RC(x=str0(x);S s=xV;P(!*s,x(_R(cn[tf])))L v=pf(&s);x(*s?_R(cn[tf]):aV(tf,1,&v)))R_(et(x)))
-A1(prsC,XB(N n=xn;A u=MINE(x)?xR:aV(tB,n,xV);uT=tC;un=n+7>>3;I(n%8u,uC[un-1]&=-1+(1<<(n%8u)));x(u))et(x))
+Z X1(prsC,RmMA(e1f(prsC,x))Rc(prsC(enl(x)))RB(N n=xn;A u=MINE(x)?xR:aV(tB,n,xV);uT=tC;un=n+7>>3;I(n%8u,uC[un-1]&=-1+(1<<(n%8u)));x(u))R_(et(x)))
 Z Y2(pad,RmMA(e2f(pad,x,y))RC(K2("{y@(!x)+(x<0)*#y}",x,y))Rc(dlr(x,enl(y)))R_(et(y)))
 X2(dlr,Rs(I v=xv;P(v-(C)v,ed(y))G(&csti,cF,cC,cS,cB,prsB,prsC,prsI,prsF,ed)[si("ifcsbBCIF",v|'s'*!v)](y))Ril(pad(x,y))R_(et(y)))
 A1(sqzZ,C r=0,t=xT;U n=xN,w=Tw[t]-3;L *m=ms[w-1];I i,j;CO L*y=AL(xV);MS(xG+(n<<w),0,-n%(1u<<5-w)<<w);
