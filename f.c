265711a80e1ec431933633723212ab519429b052
Andrea Piseri <andrea.piseri@gmail.com>
2025-11-19 18:05:27 +0100

    pull out table creation, some renaming

--- f.c
+++ f.c
@@ -31,27 +31,24 @@ Z X2(fndGx,
   y(sqzZ(z))))
 
 Z W hc0(W x)_(x^=x>>33;x*=0xff51afd7ed558ccdUL;x^=x>>33;x*=0xc4ceb9fe1a85ec53UL;x^=x>>33;x) // murmur64
+Z A tbl(U n)_(W m=1ULL<<64-CLZ((W)n*9/8);A x=an(MIN(m,~0U),tI);MS(xV,-1,(4*(W)xn+31)&~31ULL);xn=m;x) // xn=0 means UINT32_MAX+1
 Z W hc(A x/*0*/)_(XP(hc0(x))XB(x=cG(xR);x(hc(x)))XE(x=gZ(xR);x(hc(x)))x=sqz(xR);I(LH(tG,xt,tL),x=sqzZ(x));W s=hc0(Lt(xt)|xn);
  XR(F(xn|!xn,s=hc0(s^hc(xa)));x(s))W n=xn<<xw-3;F(n>>3,s=hc0(s^xl))I(n&7,s=hc0(s^(xL[n>>3]&(1ULL<<8*(n&7))-1)));x(s))
-W fndA_(U *g,W n,A *x,A y)_(W h=hc(y),d=0;U i;W((i=g[h&(n-1)])!=~0U,P(mtc_(x[i],y),h&(n-1));h+=++d);h&(n-1))
-A2(fndA,Z A z=0;Z W n;Z U k;U l=urnk(y);
- P(z,P(l>k-1,r2f(fndA,x,y))P(l<k-1,er(y))U b=y(fndA_(zI,n,xA,y));(U)zI[b]<xn?az((L)(U)zI[b]):al(NL))
+W fndA_(A x,A y,A z)_(U h=hc(y),d=0,n=zn,i;W((i=zI[h&(n-1)])!=~0U,P(mtc_(xA[i],y),h&(n-1));h+=++d);h&(n-1))
+A2(fndA,Z A z=0;Z U k;U l=urnk(y);
+ P(z,P(l>k-1,r2f(fndA,x,y))P(l<k-1,er(y))U j=y(fndA_(x,y,z));(U)zI[j]<xn?az((L)(U)zI[j]):al(NL))
  k=urnk(x);P(l+1<k,er(y))
- // ugly hack: override length limit
- P(l>=k,n=1ULL<<64-CLZ(xn*5ULL/4);P(n/2-(U)(n/2),ez(y));z=an(n/2,tL);MS(zV,-1,4*n);F(xn,U b=fndA_(zI,n,xA,xa);I(zI[b]==-1,zI[b]=i));y=z(fndA(x,y)),z=0;y)
+ P(l>=k,z=tbl(xn);F(xn,U j=fndA_(x,xa,z);I(zI[j]==-1,zI[j]=i));y=z(fndA(x,y)),z=0;y)
  F(xn,P(mtc_(y,xa),y(al(i))))y(al(NL)))
-Z L eyt_(L*d,A x,L i,L k)_(P(k>xn,i)i=eyt_(d,x,i,2*k);S4(xw-3,d[k]=xg,d[k]=xh,d[k]=xi,d[k]=xl);eyt_(d,x,i+1,2*k+1))
-Z X1(eyt,R_(et(x))RE(eyt(gZ(x)))RB(eyt(cG(x)))RGHIL(A y=aL(xn+1);*yL=0;eyt_(yL,x,0,1);x(y)))
-A2(fndZFS,Z U*g=0;Z W n;
- P(g,YmMA(r2f(fndZFS,x,y))YE(fndZFS(x,gZ(y)))YB(fndZFS(x,cG(y)))Yt(fir(N(fndZFS(x,enl(y)))))
-  P(xt==TT[yt]||ytZ&&LH(tG,xt,tL),U wx=xw-3,wy=yw-3;A z=an(yn,tL);
-   F(yn,L u,v;S4(wy,u=yg,u=yh,u=yi,u=yl);W h=hc0(u),d=0,j;zl=NL;W((j=g[h&(n-1)])!=~0U,S4(wx,,v=xH[j],v=xI[j],v=xL[j]);I(u==v,zl=j;break);h+=++d))y(z))
+A2(fndZFS,Z A z=0;
+ P(z,YmMA(r2f(fndZFS,x,y))YE(fndZFS(x,gZ(y)))YB(fndZFS(x,cG(y)))Yt(fir(N(fndZFS(x,enl(y)))))
+  P(xt==TT[yt]||ytZ&&LH(tG,xt,tL),U wx=xw-3,wy=yw-3,n=zn;A u=an(yn,tL);
+   F(yn,L l,m;ul=NL;S4(wy,l=yg,l=yh,l=yi,l=yl)U h=hc0(l),d=0,j;W((j=zI[h&(n-1)])!=~0U,S4(wx,,m=xH[j],m=xI[j],m=xL[j]);I(l==m,ul=j;break);h+=++d))y(u))
   fN(y))
  Yt(P(xt==TT[yt]||ytz&&xtZ,L v=gl(y);S4(xw-3,,F(xn,P(v==xh,ai(i))),F(xn,P(v==xi,ai(i))),F(xn,P(v==xl,ai(i))))al(NL))y(al(NL)))
- n=1ULL<<64-CLZ(xn*5ULL/4);P(n/2-(U)(n/2),ez(y));A z=an(n/2,tL);g=zV;MS(g,-1,n*4);U wx=xw-3;
- F(xn,L u,v;S4(wx,,u=xh,u=xi,u=xl);W h=hc0(u),d=0,j;W((j=g[h&(n-1)])!=~0U,S4(wx,,v=xH[j],v=xI[j],v=xL[j]);I(u==v,break);h+=++d)
-  I(g[h&(n-1)]==~0U,g[h&(n-1)]=i))
- y=z(fndZFS(x,y));g=0;y)
+ z=tbl(xn);U wx=xw-3,n=zn;
+ F(xn,L l,m;S4(wx,,l=xh,l=xi,l=xl);U h=hc0(l),d=0,j;W((j=zI[h&(n-1)])!=~0U,S4(wx,,m=xH[j],m=xI[j],m=xL[j]);I(l==m,break);h+=++d)I(zI[h&(n-1)]==-1,zI[h&(n-1)]=i))
+ y=z(fndZFS(x,y));z=0;y)
 X2(fnd,
  R_(et(y))
  Rm(i1(xx,N(fnd(xy,y))))
